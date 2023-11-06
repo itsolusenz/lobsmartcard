de0ea164719c47e25c45ade9360e70be1b7e8efd
itsolusenz <itsolusenz1@gmail.com>
2023-11-06 19:35:13 +0530

    new bug fixes

--- components/front/src/app/component/header.js
+++ components/front/src/app/component/header.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { usePathname } from 'next/navigation'
 
-
+import Head from "next/head"
 
 export default function Header(props) {
   const router = usePathname();
@@ -58,6 +58,21 @@ export default function Header(props) {
 
   return (
     <>
+
+      <script
+        type='text/javascript'
+        dangerouslySetInnerHTML={{
+          __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/6548cd19f2439e1631ec21cb/1hei62bmg';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();` }}
+      />
 
 
       {/* < Helmet> */}
